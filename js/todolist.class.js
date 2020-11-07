3d4e4ebd2f7f74ac45ad7c34771a4365ccd7326c
Karina <akanetka11@gmail.com>
2020-11-07 13:08:23 +0300

    Добавляю метод создания элемента

--- js/todolist.class.js
+++ js/todolist.class.js
@@ -2,4 +2,12 @@ class ToDoList {
   constructor(list = []) {
     this.list = list;
   }
+  //создаю элемент
+  create(name, event) {
+    let id = 1;
+    if (this.list.length) {
+      id = this.list[this.list.length - 1].id + 1;
+    }
+    this.list.push({ id, name });
+  }
 }
